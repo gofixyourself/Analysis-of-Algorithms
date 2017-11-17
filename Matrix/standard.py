4442847a894b0affe470149a2e13544dc4dbb2cb
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-11-17 11:56:27 +0300

    Reformated code

--- Matrix/standard.py
+++ Matrix/standard.py
@@ -1,12 +1,12 @@
 def multiplication(matrix_a, matrix_b):
-    rows_amount_a, column_amount_a = len(matrix_a), len(matrix_a[0])
-    rows_amount_b, column_amount_b = len(matrix_b), len(matrix_b[0])
+    rows_amount_a, columns_amount_a = len(matrix_a), len(matrix_a[0])
+    rows_amount_b, columns_amount_b = len(matrix_b), len(matrix_b[0])
 
-    matrix_c = [[0 for i in range(column_amount_b)] for j in range(rows_amount_a)]
+    matrix_c = [[0 for i in range(columns_amount_b)] for j in range(rows_amount_a)]
 
     print(matrix_c)
     for i in range(rows_amount_a):
-        for j in range(column_amount_a):
+        for j in range(columns_amount_a):
             for k in range(rows_amount_b):
                 matrix_c[i][k] += matrix_a[i][j] * matrix_b[j][k]
 
@@ -17,5 +17,3 @@ if __name__ == '__main__':
     B = [[1, 1], [2, 0]]
 
     print(multiplication(A, B))
-
-
