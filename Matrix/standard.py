bf0f2b87575b3c35ede0b249eda256a389e9d1e8
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-11-17 03:52:10 +0300

    Added standard method

---
+++ Matrix/standard.py
@@ -0,0 +1,21 @@
+def multiplication(matrix_a, matrix_b):
+    rows_amount_a, column_amount_a = len(matrix_a), len(matrix_a[0])
+    rows_amount_b, column_amount_b = len(matrix_b), len(matrix_b[0])
+
+    matrix_c = [[0 for i in range(column_amount_b)] for j in range(rows_amount_a)]
+
+    print(matrix_c)
+    for i in range(rows_amount_a):
+        for j in range(column_amount_a):
+            for k in range(rows_amount_b):
+                matrix_c[i][k] += matrix_a[i][j] * matrix_b[j][k]
+
+    return matrix_c
+
+if __name__ == '__main__':
+    A = [[1, -1], [2, 0], [3, 0]]
+    B = [[1, 1], [2, 0]]
+
+    print(multiplication(A, B))
+
+
