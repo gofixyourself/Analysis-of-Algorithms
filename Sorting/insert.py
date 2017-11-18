8b6c95ca54bd6b4d10cd13fb7010c7fe63247754
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-11-18 22:14:39 +0300

    Added insertion sorting

---
+++ Sorting/insert.py
@@ -0,0 +1,9 @@
+def insertion_sorting(array):
+    length = len(array)
+
+    for i in range(1, length):
+        while i > 0 and array[i] < array[i - 1]:
+            array[i], array[i - 1] = array[i - 1], array[i]
+            i -= 1
+
+    return array
