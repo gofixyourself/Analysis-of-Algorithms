ccf05ff7d470dcb0f42f27a4b11451c717d4ab22
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-11-30 03:18:36 +0300

    Added after removing time tests

--- Sorting/bubble.py
+++ Sorting/bubble.py
@@ -3,7 +3,7 @@ def bubble_sorting(array):
 
     for i in range(length - 1, 0, -1):
         for j in range(i):
-            if array[j] > array[j + 1]:
+            if array[j] < array[j + 1]:
                 array[j + 1], array[j] = array[j], array[j + 1]
 
     return array
--- Sorting/insert.py
+++ Sorting/insert.py
@@ -2,7 +2,7 @@ def insertion_sorting(array):
     length = len(array)
 
     for i in range(1, length):
-        while i > 0 and array[i] < array[i - 1]:
+        while i > 0 and array[i] > array[i - 1]:
             array[i], array[i - 1] = array[i - 1], array[i]
             i -= 1
 
