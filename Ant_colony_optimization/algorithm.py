72118e4c322ee9bdcad1343b28ddc9411e2869dd
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-12-07 04:43:29 +0300

    Explanatory comments added

--- Ant_colony_optimization/algorithm.py
+++ Ant_colony_optimization/algorithm.py
@@ -6,9 +6,8 @@ def create_distance_matrix(cities_number):
     matrix = numpy.zeros((cities_number, cities_number))
     for i in range(cities_number):
         for j in range(i + 1, cities_number):
-            distance = randint(1, 10)
-            matrix[i][j] = distance
-            matrix[j][i] = distance
+            distance = randint(1, 10)  # from the minimum distance to the maximum
+            matrix[i][j], matrix[j][i] = distance, distance  # for symmetry of the matrix with respect to the main axis
 
     return matrix
 
