086a0f6a65970d154a37693202d749e709db1b98
Kirillova Sofia <s.kirillovaaa@gmail.com>
2017-12-07 20:24:52 +0300

    Finished Lab

--- Ant_colony_optimization/algorithm.py
+++ Ant_colony_optimization/algorithm.py
@@ -1,11 +1,12 @@
-from random import randint, random
+from random import randint
 import numpy
 
-values = {'alpha' : 1,    # pheromone weights
-          'beta' : 1,       # pheromone weights
-          'e' : 3,          # elite ants
-          'Q' : 10,         # order of the length of the optimal path
-          'p' : 0.5}
+values = {'alpha': 1,  # pheromone weights
+          'beta': 1,  # pheromone weights
+          'e': 3,  # elite ants
+          'Q': 10,  # order of the length of the optimal path
+          'p': 0.5}  # pheromone evaporation
+
 
 def create_distance_matrix(cities_number):
     matrix = numpy.zeros((cities_number, cities_number))
@@ -16,6 +17,57 @@ def create_distance_matrix(cities_number):
 
     return matrix
 
-if __name__ == "__main__":
 
+def ant_colony_optimization(distance_matrix, alpha, beta, e, Q, p, cities_number):
+    shortest_route, length_route = None, None  # shortest route and its length
+    pheromons = numpy.random.sample((cities_number, cities_number))
+    visibility = 1 / distance_matrix
+
+    time = 0
+
+    while time < 300:
+        for_all_ants = numpy.zeros((cities_number, cities_number))
+
+        # for every ant:
+        for k in range(cities_number):
+            ant_town, length_to_town = [k], 0
+            current_town = k
+            while len(ant_town) != cities_number:
+                desired_cities = [r for r in range(cities_number)]
+                for visited_town in ant_town:
+                    desired_cities.remove(visited_town)
+                probability = [0 for t in desired_cities]
+                for j in desired_cities:
+                    if distance_matrix[current_town][j] != 0:
+                        temp = sum(
+                            (pheromons[current_town][l] ** alpha) * (visibility[current_town][l] ** beta) for l in
+                            desired_cities)
+                        probability[desired_cities.index(j)] = \
+                            (pheromons[current_town][j] ** alpha) * (visibility[current_town][j] ** beta) / temp
+                    else:
+                        probability[desired_cities.index(j)] = 0
+
+                max_probability = max(probability)
+                if max_probability == 0:
+                    return "Current ant is isolated!"
+
+                selected_city = probability.index(max_probability)
+                ant_town.append(desired_cities[selected_city])
+                length_to_town += distance_matrix[current_town][desired_cities[selected_city]]
+                current_town = desired_cities.pop(selected_city)
+            if length_route is None \
+                    or (length_to_town + distance_matrix[ant_town[0]][ant_town[-1]]) < length_route:
+                length_route = length_to_town + distance_matrix[ant_town[0]][ant_town[-1]]
+                shortest_route = ant_town
+
+            for pheromone in range(len(ant_town) - 1):
+                a = ant_town[pheromone]
+                b = ant_town[pheromone + 1]
+                for_all_ants[a][b] += Q / length_to_town
+
+        for_elite = (e * Q / length_route) if length_route else 0
+        pheromons = (1 - p) * pheromons + for_all_ants + for_elite
+        time += 1
+
+    return shortest_route, length_route
 
